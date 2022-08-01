b2f1740ffc40f97b72af60e65ba695ab68dd202d
lteranl <jjteran2018@gmail.com>
2022-07-31 19:45:11 -0700

    shapes rotate on mouse move

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let isModalOpen = false;
 
+const scaleFactor = 1 / 20;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
@@ -30,4 +32,19 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly on email@email.com"
             );
         });
+}
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${
+            y * boolInt
+        }px) rotate(${x * boolInt * 10}deg)`;
+    }
 }
