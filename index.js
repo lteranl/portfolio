3f75bebb7d413dee3f943fdb07c7f76d91a26971
jesus <jjteran2018@gmail.com>
2022-08-02 16:52:22 -0700

    formated files

--- index.js
+++ index.js
@@ -6,70 +6,70 @@ let landingPage = document.querySelector("#landing-page ");
 let body = document.querySelector("body");
 
 function toggleModal() {
-  if (isModalOpen) {
-    isModalOpen = false;
-    return document.body.classList.remove("modal--open");
-  }
-  isModalOpen = true;
-  document.body.classList += "modal--open";
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += "modal--open";
 }
 
 function toggleContrast() {
-  contrastToggle = !contrastToggle;
-  if (contrastToggle) {
-    document.body.classList += " dark-theme";
-  } else {
-    document.body.classList.remove("dark-theme");
-  }
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
 }
 
 function contact(event) {
-  event.preventDefault();
-  const loading = document.querySelector(".modal__overlay--loading");
-  const success = document.querySelector(".modal__overlay--success");
-  loading.classList += " modal__overlay--visible";
-  emailjs
-    .sendForm(
-      "service_7ilvy6g",
-      "template_1uk5of3",
-      event.target,
-      "user_OSQVwssVSVVqaMT0U7GdG"
-    )
-    .then(() => {
-      loading.classList.remove("modal__overlay--visible");
-      success.classList += " modal__overlay--visible";
-    })
-    .catch(() => {
-      loading.classList.remove("modal__overlay--visible");
-      alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
-      );
-    });
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            "service_7ilvy6g",
+            "template_1uk5of3",
+            event.target,
+            "user_OSQVwssVSVVqaMT0U7GdG"
+        )
+        .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        })
+        .catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+            );
+        });
 }
 
 function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
 
-  for (let i = 0; i < shapes.length; i++) {
-    const isOdd = i % 2 !== 0;
-    const boolInt = isOdd ? -1 : 1;
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
 
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${
-      y * boolInt
-    }px) rotate(${x * boolInt * 10}deg)`;
-  }
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${
+            y * boolInt
+        }px) rotate(${x * boolInt * 10}deg)`;
+    }
 }
 
 function screenLock() {
-  lockScreen.classList += " lock";
-  lockScreen.classList.remove("animate__fadeOut");
-  body.style.overflow = "hidden";
+    lockScreen.classList += " lock";
+    lockScreen.classList.remove("animate__fadeOut");
+    body.style.overflow = "hidden";
 }
 function screenUnlock() {
-  lockScreen.classList += " animate__fadeOut";
-  landingPage.style.opacity = "1";
-  lockScreen.classList.remove("lock");
-  body.style.overflow = "scroll";
+    lockScreen.classList += " animate__fadeOut";
+    landingPage.style.opacity = "1";
+    lockScreen.classList.remove("lock");
+    body.style.overflow = "scroll";
 }
