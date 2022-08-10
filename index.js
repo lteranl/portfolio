c6de4a5b826e6c03b7e7627c00ae7d85df27fd79
lteranl <jjteran2018@gmail.com>
2022-08-10 16:22:30 -0700

    navbar toggle function added

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const scaleFactor = 1 / 20;
 let lockScreen = document.querySelector(".lockscreen");
 let landingPage = document.querySelector("#landing-page ");
 let body = document.querySelector("body");
+const nav = document.getElementById("top__nav");
+const navButton = document.querySelector("button[aria-expanded]");
+
+window.onscroll = function () {
+    if (window.pageYOffset > 395) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+};
+function toggleNav({ target }) {
+    const expanded = target.getAttribute("aria-expanded") === "true" || false;
+
+    navButton.setAttribute("aria-expanded", !expanded);
+}
+navButton.addEventListener("click", toggleNav);
 
 function toggleModal() {
     if (isModalOpen) {
